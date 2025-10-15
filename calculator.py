1a3f37833b41738ae136a75b95bf285d0ddcd895
N00RULAMEEN <nooru36124@gmail.com>
2025-10-15 17:14:04 +0530

    Auto daily update

---
+++ calculator.py
@@ -0,0 +1,60 @@
+"""
+Today Learn Maths Related such as + , -, / , % ,
+Just See Calculation steps
+"""
+
+x = float(input("What's X value ? "))
+y = float(input("What's Y value ? "))
+z = x / y
+
+print(f"{z:.2f}")
+
+"""
+x = float(input("What's X value ? "))
+y = float(input("What's Y value ? "))
+z = x / y
+print(z)
+
+"""
+
+
+
+
+"""
+# Addition Problems & Basics
+x = float(input("What's X value ? "))
+y = float(input("What's Y value ? "))
+z = round(x) + (y)
+print(f"{z:,}")
+"""
+"""
+#so in this code there will no commas to add see above steps
+x = float(input("What's X value ? "))
+y = float(input("What's Y value ? "))
+z = round(x)+(y)
+print("The sum is : ",z)
+"""
+"""
+# the code is too lenghty instead yu could use nesting functions as below
+x = int(input("What's X value ? "))
+y = int(input("What's Y value ? "))
+print("The sum is : ", x + y)
+"""
+"""
+# This will print the sum
+
+x = input("What's X value ? ")
+y = input("What's Y value ? ")
+z = int(x) +int(y)
+print("The sum is : ", z)
+
+"""
+
+# The Below Code Will not cacluate instead it does put together the value
+"""
+x = input("What's X value ? ")
+y = input("What's Y value ? ")
+
+z = x + y
+print("The sum is : ", z)
+"""
