0b461b22abd5897e423073dd85696fc055cd898f
N00RULAMEEN <nooru36124@gmail.com>
2025-10-15 17:40:46 +0530

    Auto daily update

---
+++ helloo.py
@@ -0,0 +1,42 @@
+
+def main():
+     x = int(input("What's x? "))
+     print("x squared is", square(x))
+
+def square(n):
+     return n * n
+
+main()
+
+
+
+
+"""
+def hello(to):
+ print("hello,", to)
+
+name = input("What's your name? ")
+hello(name)
+
+"""
+"""
+
+def hello():
+ print("hello")
+
+name = input("What's your name? ")
+hello()
+print(name)
+
+"""
+
+'''
+def hello(to="world"):
+print("hello,", to)
+
+hello()
+name = input("What's your name? ")
+hello(name)
+
+
+'''
