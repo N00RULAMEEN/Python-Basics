a0d1d998de295153aacca296a8d66d21b2f95bb2
N00RULAMEEN <nooru36124@gmail.com>
2025-10-22 12:54:50 +0530

    Auto daily update

---
+++ compare.py
@@ -0,0 +1,12 @@
+# HERE WE JUST LEARNING CONDITIONS FORM BLOCK 2
+# not explaining just going on
+
+x = int(input("What's X? :"))
+y =  int(input("What's Y? :"))
+
+if x <= y:
+    print("X is Less than y")
+elif x >= y:
+    print("X is Greater than y")       
+else :
+    print("x = Y")    
