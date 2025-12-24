c9013036e64020aae5b9c4c6bf7b0292581cee69
N00RULAMEEN <nooru36124@gmail.com>
2025-12-24 12:19:08 +0530

    Auto daily update

---
+++ results.py
@@ -0,0 +1,15 @@
+results = ["Mario", "Luigi"]
+
+
+results.append("Princess")
+results.append("Yoshi")
+results.append("Koopa Troopa")
+results.append("Toad")
+results.append(["Bowser", "Donkey Kong Jr."])
+results.insert(0,"David")
+results.reverse()
+results.remove("Yoshi")
+
+
+print(results)
+
