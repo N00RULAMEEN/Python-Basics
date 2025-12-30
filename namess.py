d46d92908c9e15fb5fd63e20d18c4ca7d9abb54a
N00RULAMEEN <nooru36124@gmail.com>
2025-12-30 12:48:55 +0530

    Auto daily update

--- namess.py
+++ namess.py
@@ -1,6 +1,106 @@
+import csv
+
+name = input("What's your name? ")
+home = input("Where's your Home? ")
 
 
+with open("Students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames = ["name", "home"])
+    writer.writerow({"name": name, "home" :home})
 
+
+"""
+import csv
+
+students = []
+
+with open("namess.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name" :row["name"], "home": row["home"]})
+
+for student in sorted(students, key = lambda student: student["name"], reverse= True):
+    print(f"{student['name']} is from {student['home']}")  
+
+"""
+"""
+import csv
+
+students = []
+
+with open("namess.csv") as file:
+    reader = csv.reader(file)
+    for name,home in reader:
+        students.append({"name" : name, "home": home})
+
+for student in sorted(students, key = lambda student: student["name"], reverse= True):
+    print(f"{student['name']} is from {student['home']}")  
+
+"""
+"""
+students = []
+
+with open("names.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        student = {"name" : name, "house" : house}
+        students.append(student) 
+
+for student in sorted(students, key = lambda student: student["name"], reverse= True):
+    print(f"{student['name']} is on {student['house']}")      
+      """
+
+"""
+students = []
+
+with open("names.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        student = {"name" : name, "house" : house}
+        students.append(student)
+
+def get_name(student):
+    return student["name"]        
+
+for student in sorted(students, key = get_name, reverse= True):
+    print(f"{student['name']} is on {student['house']}")        
+
+# To Reverse House 
+#def get_House(student):
+ #   return student["house"]        
+
+#for student in sorted(students, key = get_House, reverse= True):
+ #   print(f"{student['name']} is on {student['house']}")        
+
+"""
+"""
+students = []
+
+with open("names.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        student = {"name" : name, "house" : house}
+        students.append(student)
+
+for student in students:
+    print(f"{student['name']} is on {student['house']}")        
+
+"""
+"""
+students = []
+
+with open("names.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        student = {}
+        student["name"] = name
+        student["house"] = house
+        students.append(student)
+
+for student in students:
+    print(f"{student['name']} is on {student['house']}")        
+
+"""
 
 """
 students = []
